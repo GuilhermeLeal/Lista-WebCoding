551344f89c51b4b7d6961d1fb546263f2b7485db
GuilhermeLeal <guilhermegordilho@gmail.com>
2023-06-01 23:38:25 -0300

    lista web coding

---
+++ index.js
@@ -0,0 +1,98 @@
+
+//PROFESSOR, VOU COLOCAR A LISTA DOS 4 EXERCÍCIOS A SEGUIR, QUANDO O SENHOR FOR TESTAR, SÓ TIRAR DO COMENTÁRIO.
+// A SEGUIR ESTÃO OS 4 EXERCÍCIOS.
+
+
+
+//CLASSE 1
+
+
+/*
+     class Empregado {
+     constructor(nome, salario) {
+       this.nome = nome;
+       this.salario = salario;
+     }
+  
+     aumentarSalario() {
+       this.salario *= 1.1;
+     }
+   }
+  
+   const empregado = new Empregado('Vinicius', 2537);
+   console.log('Salário inicial:', empregado.salario);
+   empregado.aumentarSalario();
+   console.log('Novo salário:', empregado.salario);
+   console.log('Seu nome:', empregado.nome);
+   */
+
+
+  //CLASSE 2 
+
+
+  /*
+   class Produto {
+     constructor(nome, descricao, preco) {
+       this.nome = nome;
+       this.descricao = descricao;
+       this.preco = preco;
+     }
+  
+     aumentarPreco(valor) {
+       this.preco += valor;
+     }
+   }
+  
+   const produto1 = new Produto('Produto 1', 'Descrição do produto 1', 400);
+   console.log('Preço inicial:', produto1.preco);
+   produto1.aumentarPreco(87);
+   console.log('Novo preço:', produto1.preco);
+    */
+
+
+  //CLASSE 3
+
+
+  /* 
+  class Animal {
+     constructor(nome) {
+       this.nome = nome;
+     }
+  
+     falar() {
+       console.log(`${this.nome} faz barulho.`);
+     }
+   }
+  
+   class Cachorro extends Animal {
+     latir() {
+       console.log('Au au!');
+     }
+   }
+  
+   const cachorro1 = new Cachorro('Rex');
+   cachorro1.latir();
+   */
+
+   
+  //CLASSE 4
+
+
+   /*
+    class ContaBancaria {
+    constructor(titular, saldo, limite) {
+    this.titular = titular;
+    this.saldo = saldo;
+    this.limite = limite;
+   }
+  
+     depositar(valor) {
+       this.saldo += valor;
+     }
+   }
+  
+    const conta1 = new ContaBancaria('Fulano', 1000, 500);
+    conta1.depositar(500);
+    console.log(conta1.saldo); 
+    */
+    
